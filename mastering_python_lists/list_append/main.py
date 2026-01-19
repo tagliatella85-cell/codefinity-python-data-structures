f37cd3baf956e3ec93f1265554a620f57d6bf644
tagliatella85-cell <>
2026-01-19 06:32:03 +0000

    List append

---
+++ mastering_python_lists/list_append/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ['Paris', 'Tokyo']
+
+# Write your code below
+travel_wishlist.append("Berlin")
+travel_wishlist.append("New York")
+
+# Testing
+print("Updated list:", travel_wishlist)
