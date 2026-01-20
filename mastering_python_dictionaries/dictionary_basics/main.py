abe8e0b42ff5aed07255747fd3ac364bd239b2be
tagliatella85-cell <>
2026-01-20 06:24:30 +0000

    Dictionary Basics

---
+++ mastering_python_dictionaries/dictionary_basics/main.py
@@ -0,0 +1,12 @@
+library = {
+    "The Great Gatsby": "F. Scott Fitzgerald",
+    "1984": "George Orwell",
+    "To Kill a Mockingbird": "Harper Lee"
+}
+print("Library dictionary:", library)
+
+
+
+
+
+ 
